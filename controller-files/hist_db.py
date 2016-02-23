f2a5a5a2d48a6d49c824b037353c22c57b77797e
Amos Smith <als0062@auburn.edu>
2016-02-23 13:14:24 -0600

    hist_db

---
+++ controller-files/hist_db.py
@@ -0,0 +1,10 @@
+from createDeviceChain import createChain 
+from sql_query import queryValue
+import time, os, numpy, subprocess
+
+tablename == "controllerone"
+columnname == "tempOA"
+path = 'sqlite:///../database/database/rh.db'
+
+value = queryValue(tablename,columnname,path)
+print(value)
